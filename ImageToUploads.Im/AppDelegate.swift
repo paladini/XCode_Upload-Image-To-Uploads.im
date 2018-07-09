1f9bbc2bf2444a831bb718640936e63841be6a88
Fernando Paladini <fnpaladini@gmail.com>
2018-07-09 19:27:45 -0300

    Initial Commit

---
+++ ImageToUploads.Im/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageToUploads.Im
+//
+//  Created by 1Doc Tecnologia on 09/07/18.
+//  Copyright © 2018 Fernando Paladini. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImageToUploads.Im/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageToUploads.Im
+//
+//  Created by 1Doc Tecnologia on 09/07/18.
+//  Copyright © 2018 Fernando Paladini. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
